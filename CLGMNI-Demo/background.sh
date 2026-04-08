6b65fd889b4d8a4aff576993845ade6be29510f8
Jay <jayhawkins108@gmail.com>
2026-04-07 20:49:08 -0600

    updated background.sh due to failed open

--- CLGMNI-Demo/background.sh
+++ CLGMNI-Demo/background.sh
@@ -9,8 +9,13 @@ cd /opt/clgmni
 pip3 install -r requirements.txt --break-system-packages
 
 # Create an alias for easy execution
-echo "alias clgmni='python3 /opt/clgmni/main.py'" >> ~/.bashrc
-source ~/.bashrc
+#echo "alias clgmni='python3 /opt/clgmni/main.py'" >> ~/.bashrc
+#source ~/.bashrc
+
+#Wrapper Script Method
+echo '#!/bin/bash' > /usr/local/bin/clgmni
+echo 'python3 /opt/clgmni/main.py "$@"' >> /usr/local/bin/clgmni
+chmod +x /usr/local/bin/clgmni
 
 # Signal Killercoda that the background task is complete
 touch /tmp/finished
